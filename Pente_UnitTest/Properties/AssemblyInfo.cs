ee5e921b3955289db0e1b6850bf3bcfce8f315ca
V01dPr0xy <DBevins@student.neumont.edu>
2018-05-21 20:18:15 -0600

    Added Win Conditions to be tested in the unit testing project

---
+++ Pente_UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Pente_UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Pente_UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8c7f0780-4b12-4bb6-ac2e-645273116214")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Pente_UnitTests/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pente_UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void WinColumn_True()
+        {
+            // Use the Assert class to test conditions.
+            // yield to skip a frame
+            bool expected = true;
+            int[,] board_actual = new int[9, 9];
+            for (int i = 0; i < 9; i++) for (int j = 0; j < 9; j++) board_actual[i, j] = -1;
+
+            board_actual[5, 3] = 1;
+            board_actual[5, 4] = 1;
+            board_actual[5, 5] = 1;
+            board_actual[5, 6] = 1;
+            board_actual[5, 7] = 1;
+
+            bool actual = ValidityTests.CheckForWin(board_actual, 5, 5);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
